b5dd1d3c2294c11a59010452d39737357b937865
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-02-20 07:46:10 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Word Break/word-break.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+// A : given string to search
+// B : vector of available strings
+
+class Solution
+{
+public:
+    int wordBreak(string A, vector<string> &B)
+    {
+        int n = A.length();
+
+        unordered_set<string> dict(B.begin(), B.end());
+
+        vector<bool> dp(n + 1, false);
+        dp[0] = true; 
+
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = i - 1; j >= 0; --j)
+            {
+                if (dp[j])
+                {
+                    string word = A.substr(j, i - j);
+                    if (dict.find(word) != dict.end())
+                    {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[n] ? 1 : 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> dict;
+        for(int i=0;i<n;i++){
+            string S;
+            cin>>S;
+            dict.push_back(S);
+        }
+        string line;
+        cin>>line;
+        Solution ob;
+        cout<<ob.wordBreak(line, dict)<<"\n";
+    }
+}
+
+// } Driver Code Ends
