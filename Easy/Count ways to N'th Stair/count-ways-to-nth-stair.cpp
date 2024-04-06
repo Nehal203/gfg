3d23cc901812a945a76d234472d72d3efe3758d7
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-04-06 18:03:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count ways to N'th Stair/count-ways-to-nth-stair.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    long long countWays(int n) {
+       if(n%2==0)
+       {
+           long long ans = n/2;
+           ans = ans+1;
+           return ans;
+       }
+       else
+       {
+           n= n-1;
+           long long ans = n/2;
+           ans = ans+1;
+           return ans;
+       }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    // taking count of testcases
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // taking stair count
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.countWays(n) << endl; // Print the output from our pre computed array
+    }
+    return 0;
+}
+// } Driver Code Ends
