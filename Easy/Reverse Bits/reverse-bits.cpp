5fb5469d823c3733a94d0f1933967e3ee038e36f
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-04-13 07:52:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Bits/reverse-bits.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        long long ans=0;
+        for(int i=0;i<32;i++)
+            if(x&(1<<i))ans+=(1ll<<(31-i));
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
