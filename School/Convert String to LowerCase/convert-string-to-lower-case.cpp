2013e8c024eb70edaf5797506106dd874c7507f1
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-05-25 17:14:15 +0530

    Added Solution - GfG to GitHub

---
+++ School/Convert String to LowerCase/convert-string-to-lower-case.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string toLower(string s) {
+        // code here
+        transform(s.begin(),s.end(),s.begin(),::tolower);
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.toLower(s) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
