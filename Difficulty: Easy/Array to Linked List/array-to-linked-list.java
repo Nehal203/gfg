5807d674de1afaf2a43ec22d080ab56798c83288
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-09-02 11:12:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array to Linked List/array-to-linked-list.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node() {
+        this.data = 0;
+        this.next = null;
+    }
+
+    Node(int d) {
+        this.data = d;
+        this.next = null;
+    } // constructor to create a new node
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+        while (t-- > 0) {
+            String line = scanner.nextLine();
+            String[] numStrings = line.split(" ");
+            int[] arr = new int[numStrings.length];
+            for (int i = 0; i < numStrings.length; i++) {
+                arr[i] = Integer.parseInt(numStrings[i]);
+            }
+            Solution ob = new Solution();
+            Node ans = ob.constructLL(arr);
+            while (ans != null) {
+                System.out.print(ans.data + " ");
+                ans = ans.next;
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
+
+//Back-end complete function Template for Java
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+}
+*/
+class Solution {
+    static Node constructLL(int arr[]) {
+        // code here
+        Node head=new Node(-1);
+        Node temp=head;
+        for(int i=0;i<arr.length;i++){
+            temp.next=new Node(arr[i]);
+            temp=temp.next;
+        }
+        return head.next;
+    }
+}
