d713c1c3c752ed955c59c2321d1551ad014b3f67
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-09-04 21:26:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Height Tree/maximum-height-tree.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int N){
+        // code here
+        return ((sqrt(N*8+1))-1)/2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
