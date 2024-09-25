a012115b6f5c2d2a11b24cf42c98917f774673ff
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-09-25 15:37:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort The Array/sort-the-array.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine().trim());
+
+        while (t-- > 0) {
+            String line = scanner.nextLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            Solution obj = new Solution();
+            int[] ans = obj.sortArr(nums);
+            for (int x : nums) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    int[] sortArr(int[] arr) {
+        // code here
+        // for(int i=0;i<arr.length-1;i++){
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(arr[i]>arr[j]){
+        //             int temp=arr[i];
+        //             arr[i]=arr[j];
+        //             arr[j]=temp;
+        //         }
+        //     }
+        // }
+        // return arr;
+        Arrays.sort(arr);
+        return arr;
+    }
+}
