6ca713e8868e8a777abfa55c6d2c775ea2e49e23
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-09-26 22:47:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiplication Table/multiplication-table.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N = sc.nextInt();
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.getTable(N);
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static ArrayList<Integer> getTable(int N){
+        // code here
+        ArrayList <Integer> result=new ArrayList<>();
+        for(int i=1;i<=10;i++){
+            result.add(N*i);
+        }
+        return result;
+    }
+}
+
+
