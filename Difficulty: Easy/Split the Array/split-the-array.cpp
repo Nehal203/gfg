70c56d6f26d0668f2a0efc801e9c5cf5b9358833
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2024-10-21 16:44:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split the Array/split-the-array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int countgroup(vector<int>& arr) {
+        // Complete the function
+        int x = 0;
+        for(int i : arr)
+            x ^= i;
+        if(x != 0)
+            return 0;
+        
+        long long a = pow(2, arr.size()-1) - 1;
+        long long mod = pow(10, 9)+7;
+        
+        return a%mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int ans = obj.countgroup(arr);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
