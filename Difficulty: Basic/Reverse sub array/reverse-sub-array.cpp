0cce7e29f5b62f6bfe43d8d8e2216559b4550574
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2025-03-02 10:56:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse sub array/reverse-sub-array.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+    vector<int> reverseSubArray(vector<int> &arr, int l, int r) {
+        // code here
+        reverse(arr.begin()+(l-1),arr.begin()+(r));
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        string ls;
+        getline(cin, ls);
+        int l = stoi(ls);
+        string rs;
+        getline(cin, rs);
+        int r = stoi(rs);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.reverseSubArray(arr, l, r);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
