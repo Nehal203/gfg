02dec5c38c67c41d7b38048c3c525f4bbefb22f2
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2025-08-20 19:13:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The dice problem/the-dice-problem.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+class Solution {
+  public:
+    int oppositeFaceOfDice(int n) {
+        // Write Your Code here
+        int ans=7-n;
+        return ans;
+    }
+};
