668834945845f9e777d0c34e03e396b854db5503
Nehal jain <118966129+Nehal203@users.noreply.github.com>
2025-08-25 19:04:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Simple Interest/simple-interest.cpp
@@ -0,0 +1,10 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    double simpleInterest(int P, int R, int T) {
+        // code here
+        double r=(P*R*T)/100.0;
+        return r;
+    }
+};
